0c7d2a77adba43e6b8dfe38733826fd4b1d288e9
Scott Fones <scott.fones@gmail.com>
2020-10-15 15:41:10 -0400

    first working implementation

---
+++ search_breadthfirst.jl
@@ -0,0 +1,41 @@
+include("specfuncs.jl")
+include("searchtree.jl")
+
+function breadthfirstsearch(goal, puzzle)
+    """Perform breadth first search.
+
+    Return solution or nothing.
+    """
+    node = newtree(puzzle);
+
+    if node.state == goal
+        return node
+    end
+
+    frontier = [node];
+    reached = [node.state];
+
+    while !isempty(frontier)
+        node = popfirst!(frontier);
+
+        states = expand(node.state);
+        acts = possibleactions(node.state);
+
+        for (cstate, caction) in zip(states, acts)
+            if cstate == goal
+                solvnode = addnode(caction, node, cstate);
+                printsolve(solvnode)
+                return solvnode
+            end
+
+            if isnothing(findfirst(isequal(cstate), reached))
+                push!(reached, cstate);
+                cnode = addnode(caction, node, cstate);
+                push!(frontier, cnode)
+            end
+        end
+    end
+
+    println("No solutions found.")
+    return nothing
+end
