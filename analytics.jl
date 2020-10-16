f8e44801d1143eb00fc851b76c2149a9e435d3e3
Scott Fones <scott.fones@gmail.com>
2020-10-16 16:05:24 -0400

    added profiling as outlined in writeup

--- analytics.jl
+++ analytics.jl
@@ -16,17 +16,74 @@ puzzle3 = getpuzzle8(3);
 puzzle5 = getpuzzle8(5);
 puzzle6 = getpuzzle8(6);
 
-idfs = @benchmark iterativedfs(goal, puzzle0);
-bfs = @benchmark breadthfirstsearch(goal, puzzle0);
-astar = @benchmark astarsearch(goal, heurmanhattan, puzzle0)
+idfs = @benchmark iterativedfs($goal, $puzzle0);
+bfs = @benchmark breadthfirstsearch($goal, $puzzle0);
+astar = @benchmark astarsearch($goal, $heurmanhattan, $puzzle0)
 
-p2misplaced = @benchmark astarsearch(goal, heurmisplaced, puzzle2);
-p2manhattan = @benchmark astarsearch(goal, heurmanhattan, puzzle2);
+p2misplaced = @benchmark astarsearch($goal, $heurmisplaced, $puzzle2);
+p2manhattan = @benchmark astarsearch($goal, $heurmanhattan, $puzzle2);
 
-Profile.init(delay=.0000001)
-
+Profile.init(delay=.000001)
 
 @profile astarsearch(goal, heurmanhattan, puzzle2);
+open("astar2.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (24, 500)), format=:flat)
+end
+Profile.clear()
+
+@profile astarsearch(goal, heurmanhattan, puzzle3);
+open("astar3.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (24, 500)), format=:flat)
+end
+Profile.clear()
+
+@profile astarsearch(goal, heurmanhattan, puzzle4);
+open("astar4.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (24, 500)), format=:flat)
+end
+Profile.clear()
+
+@profile astarsearch(goal, heurmanhattan, puzzle5);
+open("astar5.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (24, 500)), format=:flat)
+end
+Profile.clear()
+
+@profile astarsearch(goal, heurmanhattan, puzzle6);
+open("astar6.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (24, 500)), format=:flat)
+end
+Profile.clear()
+
+@profile breadthfirstsearch(goal, puzzle2);
+open("breadth2.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (24, 500)), format=:flat)
+end
+Profile.clear()
+
+@profile breadthfirstsearch(goal, puzzle3);
+open("breadth3.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (24, 500)), format=:flat)
+end
+Profile.clear()
+
+@profile breadthfirstsearch(goal, puzzle4);
+open("breadth4.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (24, 500)), format=:flat)
+end
+Profile.clear()
+
+@profile breadthfirstsearch(goal, puzzle5);
+open("breadth5.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (24, 500)), format=:flat)
+end
+Profile.clear()
+
+@profile breadthfirstsearch(goal, puzzle6);
+open("breadth6.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (24, 500)), format=:flat)
+end
+Profile.clear()
 
 @show idfs;
 @show bfs;
